4f2f42195f44746e1576cee8f91e0a8e8b82abd1
Maxim <buldov@mail.ru>
2021-08-01 10:52:37 -0700

    Login page

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button';
+export { default as ShadowBox } from './ShadowBox';
+export { default as LoginForm } from './LoginForm';
+export { default as RegisterForm } from './RegisterForm';
